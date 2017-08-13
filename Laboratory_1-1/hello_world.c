86581c88cc4489db31e1938d1888fbe97ed07068
james garzon <james@experimentality.co>
2017-08-13 18:23:06 -0500

    Create hello world example

---
+++ Laboratory_1-1/hello_world.c
@@ -0,0 +1,15 @@
+
+/* Hello World Program
+ * This is a multiline comment
+ * It must start with (slash)(asterisk)
+ * and must end with (asterisk)(slash)
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    // single line comment
+    printf( "Hola mundo.\n" );
+    return 0;
+}
